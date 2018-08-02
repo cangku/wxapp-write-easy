320ff5d480b9851a8213d01e9e2e1f92b4b8a75d
unofficial <cangku@unofficial.cn>
2018-08-02 20:34:05 +0800

    init miniprogram plugin

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,12 @@
+// 以插件的形式使用
+let hello = requirePlugin('doctorwork');
+hello();
+
+// 以npm形式使用
+const util = require('@unofficial/utils');
+util();
+
+const demo = require('demo');
+demo();
+
+Page({})
---
+++ packages/plugins/doctorwork/index.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+    console.log("plugin index");
+}
